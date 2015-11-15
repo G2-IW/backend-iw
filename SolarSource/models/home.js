df41b56f1e63569690486d36da19bc287b63a651
Graham Turk <gturk9@gmail.com>
2015-11-15 07:00:41 -0500

    Simplify home schema and delete engine module

--- SolarSource/models/home.js
+++ SolarSource/models/home.js
@@ -2,21 +2,33 @@
  * Created by grahamturk on 10/22/15.
  */
 
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-var homeSchema = mongoose.Schema({
-    address: String,
-    energy_score: Number,
-    roof_score: Number
+
+
+var recommendationSchema = mongoose.Schema({
+    area: {
+        units: String,
+        value: Number
+    },
+    power_output: {
+        units: String,
+        value: Number
+    }
 });
 
-homeSchema.methods.formatHome = function() {
-    return {
-        address: this.address,
-        energy_score: this.energy_score,
-        roof_score: this.roof_score
+
+var homeSchema = mongoose.Schema({
+    lat: Number,
+    lon: Number,
+    energy: {},
+    roof: {},
+    recommendation: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'recommendationSchema'
     }
-};
+});
+
 
 var Home = mongoose.model('Home', homeSchema);
 
