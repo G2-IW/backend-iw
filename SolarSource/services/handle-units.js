0956a1cdd30111e763f755593e08a14d022b9dfa
Graham Turk <gturk9@gmail.com>
2015-11-15 06:52:12 -0500

    Zipped bug from array and clarify unit conversion

--- SolarSource/services/handle-units.js
+++ SolarSource/services/handle-units.js
@@ -10,19 +10,20 @@ var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
 
 var CONVERSION = .3048;
 
-function convertUnits(unitType, value) {
-    if (unitType === 'sqft') {
+function convertUnits(unitFrom, unitTo, value) {
+    if (unitFrom === 'sqft' && unitTo == 'sqm') {
        return value * (Math.pow(CONVERSION, 2));
-    } else if (unitType = 'sqm') {
+    } else if (unitFrom = 'sqm' && unitTo == 'sqft') {
         return value / (Math.pow(CONVERSION, 2));
     }
 
-    return convert(value).from(unitType).to('m');
+    return convert(value).from(unitFrom).to(unitTo);
 }
 
+// geocoder returns array of responses so have to do res[0]
 function getZipCode(lat, lon) {
     return geocoder.reverse({lat: lat, lon: lon}).then(function(res) {
-        return res.zipcode;
+        return res[0].zipcode;
     });
 }
 
