f8cb0867a035151d238323a0df46010eee20ba82
Graham Turk <gturk9@gmail.com>
2015-11-22 14:46:26 -0500

    Update config and package

--- SolarSource/config.js
+++ SolarSource/config.js
@@ -2,21 +2,40 @@
  * Created by grahamturk on 10/22/15.
  */
 
+module.exports.url = 'http://localhost:3000';
+
 module.exports.nrel = {
     endpoint_prefix: 'https://developer.nrel.gov',
-    api_key: 'gCScRN5rrK50DE2xoZcLpSoyFX3Nm1U2b0e6zVap'
+    api_key: 'gCScRN5rrK50DE2xoZcLpSoyFX3Nm1U2b0e6zVap',
+    pvdaq: {
+        username: 'gturk',
+        password: 'y5a24Nj9hb7h63fXt'
+    }
 };
 
 module.exports.mongodb = {
-    server_path: "mongodb://localhost/test"
+    server_path: 'mongodb://localhost/test'
 };
 
 module.exports.flags = {
     // ["wattvision", "monthly"]
-    energy: "monthly",
+    energy: 'monthly',
     peak: 200,
     average: 100,
     startFromCapacity: false,
     threshold: .05,
-    inflation: 1.05
+    inflation: 1.03,
+    apr: .05
+};
+
+module.exports.enphase = {
+    redirect_url: module.exports.url + '/enphase/callback',
+    sample_user_id: '4d7a45774e6a41320a',
+    api_key: '8807c367eda391b29447301c3cb627be',
+    authorization_url: 'https://enlighten.enphaseenergy.com/app_user_auth/new?app_id=1409612302941',
+    endpoint: 'https://api.enphaseenergy.com/api/v2'
+};
+
+module.exports.wattvision = {
+    endpoint_prefix: 'https://www.wattvision.com/api/v0.2/elec'
 };
